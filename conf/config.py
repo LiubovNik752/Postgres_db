d62f9f0e093cb77f4f4262f072d764de14987d60
Lyubov Baturina <121640778+LiubovNik752@users.noreply.github.com>
2023-09-24 18:54:12 +0300

    add config

--- conf/config.py
+++ conf/config.py
@@ -2,4 +2,4 @@ host = "psql-mock-database-cloud.postgres.database.azure.com"
 user = 'hbyhoebbrmlfqfvrhdcqfvro@psql-mock-database-cloud'
 password = 'otbqdvbxgfdaukzcppehjidx'
 db_name = 'booking1694785320901fkltepqmnliudxze'
-port = 5432
+port = '5432'
